0d231bd81ab953d6b707e321e30d6d1d73fa285e
VEZcoding <vivian.erklavec@gmail.com>
2021-03-09 16:50:28 +0100

    Mapped pathways excluding Fucosylation, which will come next

--- Glycosylation_mapping_reactions.py
+++ Glycosylation_mapping_reactions.py
@@ -5,73 +5,92 @@ Created on Fri Feb 19 12:48:48 2021
 @author: VEZcoding
 """
 from pandas import DataFrame
-import re
 enzymes = ['Man'] 
 init_str = ('F%sM%sA%sG%sS%s')# %(0,i,j,z,n)) #začetni input
-reactions = [] #list_glikanov
+reactions_in = [] #list_glikanov
+reactions_out = []
 d = {}  #dictionary za dataframe
 d['Glycan_in'] = []
 d['---->'] = []
 d['Glycan_out'] = []
-mannose = [9,8,7,6,5,4,3] 
-for i in mannose :
-    add =  init_str %(0,i,0,0,0)   
-    reactions.append(add)
-    #odvzemaj manoze in dopolni z eno vejo legokock
-    if i == 5 or i == 4: 
-        for j in range(0,2):
-            #dodajaj A blocke
-            add = init_str %(0,i,j,0,0)           
-            reactions.append(add)
-            #dodajaj G blocke
-            for z in range (0,2):
-                if z > j  and z: 
-                    continue
-                else: 
-                    add = init_str %(0,i,j,z,0)                   
-                    reactions.append(add)
-                #dodajaj S blocke
-                for n in range (0,2):
-                      if n > z: 
-                          continue
-                      else: 
-                          add = init_str %(0,i,j,z,n) 
-                          reactions.append(add)
-    if i == 3:
-        #dodaj A
-        for j in range(1,5):
-            add = init_str %(0,i,j,0,0)
-            reactions.append(add)
-            #dodaj G
-            for z in range (1,5):
-                if z > j: 
-                    continue
-                else: 
-                    add = init_str %(0,i,j,z,0)
-                    reactions.append(add)
-                #dodaj S
-                for n in range (1,5):
-                      if n > z: 
-                          continue
-                      else: 
-                          add = init_str %(0,i,j,z,n)
-                          reactions.append(add)
-                          
-
-reactions_1 = reactions[:-1]  #zadnj produkt != reaktant
-reactions_2 = reactions[1:]   #začetni reaktant != produkt
-
-for i in range(0, len(reactions)-1):
-    r_1 = re.findall(r'\d+',reactions[i])
-    r_2 = re.findall(r'\d+',reactions_2[i])
-    print (r_1)
-    if r_1[4] > r_2[4]:
-        continue
-    if r_1 == r_2: 
-        continue
-    else: 
-        d['Glycan_in'].append(reactions_1[i])
-        d['---->'].append('--->')
-        d['Glycan_out'].append(reactions_2[i]) 
+mannose = [9,8,7,6,5,4]
+c = 9 
+for i in range(0,len(mannose)) :
+    add_in =  init_str %(0,c,0,0,0)
+    add_out =  init_str %(0,c-1,0,0,0)
+    d['Glycan_in'].append(add_in)
+    d['---->'].append('---->')
+    d['Glycan_out'].append(add_out)
+    reactions_in.append(add_in)
+    reactions_out.append(add_out)
+    c -= 1
+    k = 0
+    if c == 5 or c == 4:
+        while k < 1: 
+            add_in =  init_str %(0,c,k,0,0)
+            add_out = init_str %(0,c,k+1,0,0)
+            reactions_in.append(add_in)
+            reactions_out.append(add_out)
+            d['Glycan_in'].append(add_in)
+            d['---->'].append('---->')
+            d['Glycan_out'].append(add_out)
+            k+=1
+        z = 0    
+        if k == 1:
+            while z < 1: 
+                add_in =  init_str %(0,c,k,z,0)
+                add_out = init_str %(0,c,k,z+1,0)
+                reactions_in.append(add_in)
+                reactions_out.append(add_out)
+                d['Glycan_in'].append(add_in)
+                d['---->'].append('---->')
+                d['Glycan_out'].append(add_out)
+                z+=1 
+            s = 0    
+        if z == 1:
+            while s < 1: 
+                add_in =  init_str %(0,c,k,z,s)
+                add_out = init_str %(0,c,k,z,s+1)
+                reactions_in.append(add_in)
+                reactions_out.append(add_out)
+                d['Glycan_in'].append(add_in)
+                d['---->'].append('---->')
+                d['Glycan_out'].append(add_out)
+                s+=1
+    k = 0
+    if c == 3:
+        while k < 4:
+            add_in =  init_str %(0,c,k,0,0)
+            add_out = init_str %(0,c,k+1,0,0)
+            reactions_in.append(add_in)
+            reactions_out.append(add_out)
+            d['Glycan_in'].append(add_in)
+            d['---->'].append('---->')
+            d['Glycan_out'].append(add_out)
+            k+=1
+            z = 0    
+            if k != 0:
+                while z < k: 
+                    add_in =  init_str %(0,c,k,z,0)
+                    add_out = init_str %(0,c,k,z+1,0)
+                    reactions_in.append(add_in)
+                    reactions_out.append(add_out)
+                    d['Glycan_in'].append(add_in)
+                    d['---->'].append('---->')
+                    d['Glycan_out'].append(add_out)
+                    z+=1 
+                    s = 0    
+                    if z != 0:
+                        while s < z:
+                            add_in =  init_str %(0,c,k,z,s)
+                            add_out = init_str %(0,c,k,z,s+1)
+                            reactions_in.append(add_in)
+                            reactions_out.append(add_out)
+                            d['Glycan_in'].append(add_in)
+                            d['---->'].append('---->')
+                            d['Glycan_out'].append(add_out)
+                            s+=1 
+            
+        
 df = DataFrame.from_dict(d)   #ustvari dataframe  
-print(df) #izris rezultata  
+print(df) #izris rezultata 
