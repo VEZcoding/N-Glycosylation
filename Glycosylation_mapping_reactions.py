bd860b9b36a3f25936cd17c9dc213463470e932c
VEZcoding <vivian.erklavec@gmail.com>
2021-03-11 13:29:53 +0100

    Added Fucosylation

--- Glycosylation_mapping_reactions.py
+++ Glycosylation_mapping_reactions.py
@@ -5,11 +5,6 @@ Created on Fri Feb 19 12:48:48 2021
 @author: VEZcoding
 """
 """
-This program is for generating N-glycosylation reactions. You can use it as an input to cytoscape, or to generate an excel table to 
-extract useful information for predicting the outcome of kinetic reactions. This is just a starting generator, since this pathway itself is deficient. 
-Reason behind it is, that I don't know how much of this is intelectual property. For example Gene is the same as Enzyme name, because a lot of pharmaceutical
-companies use their own abbreviations for genes and enzmyes. Naming for the Gylcan structures is taken from Oxford nomenclature. And conditions for appending glycosylation blocks 
-on the unglycosylated product are from my own reasearch: doi.org/10.1002/bit.27660 and from Sphan 2015: 10.1016/j.ymben.2015.10.007
 Starting input of Unglycosylated product will be F0M0A0G0S0. Where each letter presents a glycosylation(building) block and number that follows presents the amount of
 the blocks on the glycan - F=Fucose, M=Manose, A=GlucNAc, G = Galactose, S = Sialic Acid. For example F0M3A2G1S1 glycan is afucosylated(has no fucose), 3 mannose
 2 GlucNACs and 1 Galactose and 1 Sialic Acid.  
@@ -20,9 +15,7 @@ List of enzymes present:
     - SiaT
 For more accurate N-Glycoslyation pathway we would have acknowledge more enzmyes. Like GnTIII, or GnTIV/GnTV, which can be incorporated, 
 but in my opinion it is not necessary for a basic understanding of this pathway.     
-    
-Noted. Fucosylation pathway is still in process. Will have to expand the code a bit. 
-"""
+    """
 
 from pandas import DataFrame
 enzymes = ['ManI','GalT','GnT','SiaT'] 
